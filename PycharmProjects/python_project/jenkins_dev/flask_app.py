0554e1e2f5fba58fc84e28213208ccbd80692146
ayan <ayandas100@gmail.com>
2020-06-11 21:39:52 +0530

    flask_app

---
+++ PycharmProjects/python_project/jenkins_dev/flask_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello/')
+def hello():
+    return 'Hello world!\n'
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return  f'Hi Hello {username}'
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
